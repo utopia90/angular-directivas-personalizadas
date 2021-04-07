37f2fee6f9317e6e664bd2ed168f3ee636f96e32
Martín San José de Vicente <martin@Martins-MacBook.home>
2021-04-07 12:49:39 +0200

    Directiva Estructural creada

--- src/app/directives/structural/renderizado-condicional.directive.ts
+++ src/app/directives/structural/renderizado-condicional.directive.ts
@@ -1,10 +1,37 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appRenderizadoCondicional]'
 })
 export class RenderizadoCondicionalDirective {
 
-  constructor() { }
+  // Para conocer si se muestra o no elemento
+  private mostrado: boolean = false;
+
+  // Definimos un @Input que servirá para setear el valor de mostrado a true/false
+  // y también para mostrar el contenido que disponga de esta directiva
+  // Se le va a pasar por parámetro un true o un false
+  @Input() set appRenderizadoCondicional(condicion: boolean) {
+
+    if (!condicion && !this.mostrado) {
+      // Crear un espacio (vista) con el contenido del TemplateRef
+      // Que básicamente, es el elemento (div, por ejemplo) al que se le aplica
+      // la directiva
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.mostrado = true;
+    } else if (condicion && this.mostrado){
+      // Eliminar el elemento de la vista, haremos un clear()
+      this.viewContainer.clear();
+      this.mostrado = false;
+    }
+
+  }
+
+
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef
+  ) { }
+
 
 }
